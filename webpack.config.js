4188222ccd8d565408432ecb1061f4524a5a55d2
Christina Nathan <christina.a.nate@gmail.com>
2021-09-03 16:40:40 -0700

    Setup webpack and babel and server

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require("path");
+
+module.exports = {
+  entry: "./client/src/index.jsx",
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "/client/dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
