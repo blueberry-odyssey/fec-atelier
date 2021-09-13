108e85ce45326e856161821fc4d1e4e24ee65a4b
Jeremiah <JeremiahBerndt@gmail.com>
2021-09-12 19:43:01 -0600

    completed basic server and API requests

--- client/src/components/Related-items/Related-items.jsx
+++ client/src/components/Related-items/Related-items.jsx
@@ -1,17 +1,45 @@
 import React from 'react';
 import './Related-items.css';
-import ProductCarousel from '../ProductCarousel/ProductCarousel'
+import axios from 'axios';
 
 export default class RelatedItems extends React.Component {
   constructor(props) {
     super(props)
+    //state id is placeholder var for all related products
+    this.state = {
+      id: 47421,
+      relatedItems: []
+    }
   }
+  componentDidMount() {
+    axios.get('/findRelatedItems', { params: { id: this.state.id } })
+      .then(result => {
+        console.log('client items', result)
+        this.getAllRelatedProductStyles(result)
+      })
+      .catch(err => { throw err; })
+  }
+
+  getAllRelatedProducts () {
+    axios.get('/relatedProducts', {params: productIDArray})
+      .then(data => {
+        console.log('relatedProdcuts Data', data);
+      })
+      .catch(err => {throw err;})
+  }
+
+  getAllRelatedProductStyles(productIDArray) {
+    axios.get('/relatedProducts', {params: productIDArray, styles: true})
+      .then(data => {
+        console.log('relatedStyle Data', data);
+      })
+      .catch(err => {throw err;})
+  }
+
   render() {
     return (
       <>
         <div className='related-items'>This is the start of Related Items and Comparisons</div>
-        <ProductCarousel/>
-        <ProductCarousel/>
       </>
     )
   }
