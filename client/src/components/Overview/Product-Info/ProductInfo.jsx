c175d9afbc3eed4e2df4a58ea0385d1fad601e8f
Christina Nathan <christina.a.nate@gmail.com>
2021-09-15 19:31:25 -0700

    implemented getProductDetails in overview.jsx and added state to index.jsx

--- client/src/components/Overview/Product-Info/ProductInfo.jsx
+++ client/src/components/Overview/Product-Info/ProductInfo.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import StarRating from './StarRating.jsx';
 import ItemDetails from './ItemDetails.jsx';
 import Price from './Price.jsx';
 
-const ProductInfo = function () {
-  console.log('ProductInfo component invocked!');
+const ProductInfo = function (props) {
+  console.log('ProductInfo props product_id', props);
   return (
     <div>
       <h2>Product Information: </h2>
@@ -15,5 +16,9 @@ const ProductInfo = function () {
     </div>
   );
 }
+
+ProductInfo.propTypes = {
+  product_id: PropTypes.string.isRequired
+};
 
 export default ProductInfo;
--- server/index.js
+++ server/index.js
@@ -21,7 +21,11 @@ let params = {
   headers: { Authorization: config.TOKEN },
 };
 
-app.use(express.json())
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.use(express.static('client/dist'));
 
