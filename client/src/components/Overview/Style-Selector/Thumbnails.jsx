9723b3a50a8e0e27be047fe4de1324194f613c83
Christina Nathan <christina.a.nate@gmail.com>
2021-09-21 16:07:11 -0700

    Created createRowsOf4 function

--- client/src/components/Overview/Style-Selector/Thumbnails.jsx
+++ client/src/components/Overview/Style-Selector/Thumbnails.jsx
@@ -3,9 +3,21 @@ import React from 'react';
 //displays 4 thumbnails in a row
 //displays checkmark on style selected
 //first style is default
+
+
 const Thumbnails = function (props) {
+  let rows = [];
+  const createRowsOf4 = function (array) {
+    const copy = array.slice();
+    while (copy.length) {
+      rows.push(copy.splice(0, 4));
+    }
+  }
+  createRowsOf4(props.styles);
+
+  console.log('ROWS: ', rows);
   return (
-    <div className='styles'>
+    <div className='styles' >
       {props.styles.map(function (style) {
         return <img key={style.name} src={style.photos[0]['thumbnail_url']}></img>
       })}
