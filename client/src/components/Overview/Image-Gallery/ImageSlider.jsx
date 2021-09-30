587e74c513a01d61dfc2175059cd2da06848b294
Christina Nathan <christina.a.nate@gmail.com>
2021-09-30 14:51:13 -0700

    added arrow button functionality and currentIndex

--- client/src/components/Overview/Image-Gallery/ImageSlider.jsx
+++ client/src/components/Overview/Image-Gallery/ImageSlider.jsx
@@ -3,16 +3,52 @@ import { FaArrowAltCircleRight } from 'react-icons/fa';
 import { FaArrowAltCircleLeft } from 'react-icons/fa';
 
 //props.images is an array of object with url and thumbnail url
-const ImageSlider = function ({ images }) {
-  return (
-    <div>
-      <FaArrowAltCircleLeft className='gallery-leftBtn' />
-      <FaArrowAltCircleRight className='gallery-rightBtn' />
-      {images.map(function (image, index) {
-        return (<img src={image.url} key={index} className='image'></img>)
-      })}
-    </div>
-  )
+class ImageSlider extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      length: 0,
+      currentIndex: 0
+    };
+
+    this.prevImage = this.prevImage.bind(this);
+    this.nextImage = this.nextImage.bind(this);
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    return {
+      length: props.images.length
+    }
+    return null;
+  }
+
+  //left button should decrease currentIndex
+  prevImage() {
+    if (this.state.currentIndex === 0 ?
+      this.setState({ currentIndex: this.state.length - 1 }) :
+      this.setState({ currentIndex: this.state.currentIndex - 1 }));
+  }
+
+  //right button should increase currentIndex
+  nextImage() {
+    if (this.state.currentIndex === this.state.length - 1 ?
+      this.setState({ currentIndex: 0 }) :
+      this.setState({ currentIndex: this.state.currentIndex + 1 }));
+  }
+
+  render() {
+    console.log(this.state.currentIndex);
+    return (
+      <div>
+        <FaArrowAltCircleLeft className='gallery-leftBtn' onClick={this.prevImage} />
+        <FaArrowAltCircleRight className='gallery-rightBtn' onClick={this.nextImage} />
+        {this.props.images.map(function (image, index) {
+          return (<img src={image.url} key={index} className='image'></img>)
+        })}
+      </div>
+    )
+  }
 }
 
 export default ImageSlider;
