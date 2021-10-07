5549d54abb5d326d73e201f2af12c3b57548b754
Christina Nathan <christina.a.nate@gmail.com>
2021-10-06 18:31:11 -0700

    added js uglification/minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 var path = require('path');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
   entry: './client/src/index.jsx',
   output: {
     filename: 'bundle.js',
